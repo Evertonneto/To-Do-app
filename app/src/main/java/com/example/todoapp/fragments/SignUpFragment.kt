1426a9dd8d7393dc0c7adedc13a948afd49acc4e
Evertonneto <evertonrneto@gmail.com>
2024-03-20 11:42:07 -0300

    second video completed

--- app/src/main/java/com/example/todoapp/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/SignUpFragment.kt
@@ -5,15 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentSignUpBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var auth:FirebaseAuth
+    private lateinit var navControl:NavController
+    private lateinit var binding : FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view: View){
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents(){
+
+        binding.textViewAuth.setOnClickListener {
+            navControl.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        binding.signUpBtn.setOnClickListener {
+            val email = binding.emailText.text.toString().trim()
+            val pass = binding.passwordText.text.toString().trim()
+            val verifyPass = binding.repasswordText.text.toString().trim()
+
+            if(email.isNotEmpty() && pass.isNotEmpty() && verifyPass.isNotEmpty()){
+                if(pass == verifyPass){
+                    auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(
+                        OnCompleteListener {
+                            if (it.isSuccessful ){
+                                Toast.makeText(context, "Registrado com sucesso!", Toast.LENGTH_SHORT).show()
+                                navControl.navigate(R.id.action_signUpFragment_to_homeFragment)
+                            }else{
+                                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        })
+                }
+                else{
+                    Toast.makeText(context,"Empty fields are not allowed", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
     }
 }
